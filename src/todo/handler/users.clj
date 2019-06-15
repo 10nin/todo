64a0095aeca38cbe9c3ee7692dd28556c473515c
Akira Ota 10nin <akira19870221@gmail.com>
2019-06-16 01:46:04 +0900

    add custom validator.

--- src/todo/handler/users.clj
+++ src/todo/handler/users.clj
@@ -5,13 +5,17 @@
             [struct.core :as st]
             [todo.boundary.users :as users]))
 
-(defn- create-from-schema []
-  {:email [st/required st/email]
-   :password [st/required st/string [st/min-count 8] [st/max-count 100]]})
+(defn- create-from-schema [db]
+  (let [unique-email {:message "already used"
+                      :optional true
+                      :validate (fn [v db]
+                                  (nil? (users/find-user-by-email db v)))}]
+    {:email [st/required st/email [unique-email db]]
+     :password [st/required st/string [st/min-count 8] [st/max-count 100]]}))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{:keys [body-params]}]
-    (if-let [errors (first (st/validate body-params (create-from-schema)))]
+    (if-let [errors (first (st/validate body-params (create-from-schema db)))]
       [::response/bad-request errors]
       (let [user-id (users/create-user db body-params)]
         [::response/created (str "/users/" user-id)]))))
